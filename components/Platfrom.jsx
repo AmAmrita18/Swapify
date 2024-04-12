65342ea173684027ea679c1731e32cb68a740a03
Amrita Gupta <amamrita0818@gmail.com>
2024-04-12 23:30:06 +0530

    completed feature, footer, hero, platform,security,statistic and testimonial section

--- components/Platfrom.jsx
+++ components/Platfrom.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Platfrom = () => {
-  return <div>Platfrom</div>;
+  return <div className="platfrom" id="about">
+    <div className="bg" >
+      <img src="assets/img/platfrom.png" alt="" />
+    </div>
+    <div className="container">
+    <div className="row">
+      <div className="col-12">
+        <div className="content">
+          <div className="row justify-content-center">
+            <div className="col-lg-9 text-center wow fadeInUp"
+              data-wow-duration="0.3s"
+              data-wow-delay="0.3s">
+              <div className="section-head">
+                <h4 className="lasthead">About Us</h4>
+                <h2 className="title">
+                  The Online Cryptocurrency Exchange Platform
+                </h2>
+                <p className="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim dolor ab recusandae voluptatibus eveniet corporis vero, perspiciatis veritatis eaque modi possimus, vel, sint aliquam suscipit. Expedita, tenetur quos? Nobis, fuga?</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>;
 };
 
 export default Platfrom;
